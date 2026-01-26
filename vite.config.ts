0e22d77c0949b00787195591089144919ba8353a
LaTars4444 <emyr.jones.0@outlook.com>
2026-01-26 00:37:35 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
-      outDir: 'dist',
+      outDir: path.resolve(__dirname, 'dist'), // Force absolute path to root/dist
       emptyOutDir: true
     },
     define: {
